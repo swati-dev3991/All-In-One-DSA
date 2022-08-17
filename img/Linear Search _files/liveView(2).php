954d5086f19de8c73f546754f534914f338caf7e
swati-dev3991 <100200033+swati-dev3991@users.noreply.github.com>
2022-08-17 09:09:39 +0530

    linSearch

---
+++ img/Linear Search _files/liveView(2).php
@@ -0,0 +1,22 @@
+
+                        try
+                        {
+                            var linksArray = '  https://live.primis.tech/content/video/hls/hls.0.12.4_3.min.js  https://live.primis.tech/content/prebid/prebidVid.6.18.0_12.min.js   https://live.primis.tech/live/liveVideo.php?vpaidManager=sekindo&s=58057&ri=6C69766553746174737C736B317B54307D7B64323032322D30382D31375F30367D7B7331363133363739307D7B433135397D7B53643364334C6D7068646D463063473970626E5175593239747D7B626368726F6D657D7B716465736B746F707D7B6F77696E646F77737D7B583634307D7B593434307D7B66337D7B4C393933377DFEFE&userIpAddr=2401%3A4900%3A4bb8%3A3793%3A65fa%3A25b2%3A78a2%3A5d81&userUA=Mozilla%2F5.0+%28Windows+NT+10.0%3B+Win64%3B+x64%29+AppleWebKit%2F537.36+%28KHTML%2C+like+Gecko%29+Chrome%2F104.0.0.0+Safari%2F537.36&debugInformation=&isWePassGdpr=1&schain=1.0%2C1%21adpushup.com%2Ce224c2b5124a33f1f8d127266731a85e%2C1&noViewableMidrollPolicy=vary&isDoublePreroll=1&autoSkipVideoSec=30&c2pWaitTime=10&sdkv=&isSinglePageFloatSupport=0&csuuid=62caa4122f159&debugInfo=16136790_&debugPlayerSession=&pubUrlDEMO=&isAsyncDEMO=0&customPlaylistIdDEMO=&sta=16136790&showLogo=0&clkUrl=&plMult=-1&schedule=eyJwcmVfcm9sbCI6MSwibWlkX3JvbGwiOltdLCJnYXAiOiJhdXRvIn0%3D&content=plembed2803osxtlnur&secondaryContent=&x=640&y=440&pubUrl=https%3A%2F%2Fwww.javatpoint.com%2Flinear-search&contentNum=1&flow_closeBtn=1&flowCloseTimeout=0&flow_closeButtonPosition=right&flow_direction=br&flow_horizontalOffset=1&flow_bottomOffset=5&impGap=1&flow_width=350&flow_height=197&videoType=flow&gdpr=0&gdprConsent=&contentFeedId=&geoLati=17.411&geoLong=78.4487&vpTemplate=9937&flowMode=seenboth&isRealPreroll=0&playerApiId=&isApp=0&ccpa=0&ccpaConsent=&subId=www.javatpoint.com'.split(' ');
+
+                            for(var l = 0; l < linksArray.length; l++)
+                            {
+                                if(linksArray[l].length > 10)
+                                {
+                                    var sc = document.createElement('script');
+                                    sc.type = 'text/javascript';
+                                    sc.async = false;
+                                    sc.src = linksArray[l];
+                                    document.head.appendChild(sc);
+                                }
+                            }
+                        }
+                        catch(e)
+                        {
+                            document.write('<script type="text/javascript" src="https://live.primis.tech/content/video/hls/hls.0.12.4_3.min.js">\x3C/script><script type="text/javascript" src="https://live.primis.tech/content/prebid/prebidVid.6.18.0_12.min.js">\x3C/script><script type=' + "'" + 'text/javascript' + "'" + ' language=' + "'" + 'javascript' + "'" + ' src="https://live.primis.tech/live/liveVideo.php?vpaidManager=sekindo&s=58057&ri=6C69766553746174737C736B317B54307D7B64323032322D30382D31375F30367D7B7331363133363739307D7B433135397D7B53643364334C6D7068646D463063473970626E5175593239747D7B626368726F6D657D7B716465736B746F707D7B6F77696E646F77737D7B583634307D7B593434307D7B66337D7B4C393933377DFEFE&userIpAddr=2401%3A4900%3A4bb8%3A3793%3A65fa%3A25b2%3A78a2%3A5d81&userUA=Mozilla%2F5.0+%28Windows+NT+10.0%3B+Win64%3B+x64%29+AppleWebKit%2F537.36+%28KHTML%2C+like+Gecko%29+Chrome%2F104.0.0.0+Safari%2F537.36&debugInformation=&isWePassGdpr=1&schain=1.0%2C1%21adpushup.com%2Ce224c2b5124a33f1f8d127266731a85e%2C1&noViewableMidrollPolicy=vary&isDoublePreroll=1&autoSkipVideoSec=30&c2pWaitTime=10&sdkv=&isSinglePageFloatSupport=0&csuuid=62caa4122f159&debugInfo=16136790_&debugPlayerSession=&pubUrlDEMO=&isAsyncDEMO=0&customPlaylistIdDEMO=&sta=16136790&showLogo=0&clkUrl=&plMult=-1&schedule=eyJwcmVfcm9sbCI6MSwibWlkX3JvbGwiOltdLCJnYXAiOiJhdXRvIn0%3D&content=plembed2803osxtlnur&secondaryContent=&x=640&y=440&pubUrl=https%3A%2F%2Fwww.javatpoint.com%2Flinear-search&contentNum=1&flow_closeBtn=1&flowCloseTimeout=0&flow_closeButtonPosition=right&flow_direction=br&flow_horizontalOffset=1&flow_bottomOffset=5&impGap=1&flow_width=350&flow_height=197&videoType=flow&gdpr=0&gdprConsent=&contentFeedId=&geoLati=17.411&geoLong=78.4487&vpTemplate=9937&flowMode=seenboth&isRealPreroll=0&playerApiId=&isApp=0&ccpa=0&ccpaConsent=&subId=www.javatpoint.com">\x3C/script>');
+                        }
+                        
